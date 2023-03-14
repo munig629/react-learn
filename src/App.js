bea483dcc778a52218d0aab9bd028d3bed9ee6e7
munig629 <munig629@gmail.com>
2023-03-14 03:05:58 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React, { Component } from "react";
+
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox Develop</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button
+          onClick={(e) => {
+            console.log("Button Click");
+          }}
+        >
+          OK
+        </button>
+      </div>
+    );
+  }
 }
